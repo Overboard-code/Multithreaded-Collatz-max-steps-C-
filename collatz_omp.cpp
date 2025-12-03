70669c8d8172b770e79690730330be37de284e18
Overboard-code <53097137+Overboard-code@users.noreply.github.com>
2025-12-03 14:31:29 -0500

    Update collatz_omp.cpp to add comma support for MINGW32

--- collatz_omp.cpp
+++ collatz_omp.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <thread>
-#include <locale>
+#include <locale> // for number commas
 #include <cstdlib>   // Required for strtoll
-#include <cerrno>    // Required for errno
-#include <climits>   // Required for LLONG_MIN, LLONG_MAX
 #include <algorithm> // For max
 // g++ -O4 -o testo collatz_omp.cpp  
 // ./testo 10000000
 // Starting Collatz calculations from 1 up to 10,000,000 with 8 threads.
 // Longest chain found for number 8,400,511 with length 685
-
 using namespace std;
+
+#if defined(__MINGW32__)  // as of 2025 MINGW does not support locale for numbers 
+struct comma_out : std::numpunct<char> {
+    char do_thousands_sep()   const { return ','; }  // separate with spaces
+    std::string do_grouping() const { return "\3"; } // groups of 3 digit
+};
+#endif
 // Function to calculate Collatz steps for a given number
-long long collatz_steps(long long x)
- {
-  long long c=0;
- 
-  while (x!=1)
-   {
-    if (x&1) x=(x << 1) - (x >> 1), c+=2;
-    while (!(x&1)) x>>=1, c++;
+long long collatz_steps(long long x) {
+  long long c = 0;
+  while (x!=1) {
+   while (x&1) {
+	   x=(x << 1) - (x >> 1);
+	   c+=2; // (x*3+1)/2 count extra step 
    }
+   while (!(x&1)) x>>=1, c++; // x/2 until odd
+  }
   return c;
  }
 
@@ -41,12 +45,17 @@ void find_max_collatz(long long start_n, long long end_n, long long& max_n_local
 
 int main(int argc, char** argv) { 
     char* endp;
+#if defined(__MINGW32__) // MINGW does not support locales  Roll your own above
+	cout.imbue(locale(cout.getloc(), new comma_out));
+#else
+	cout.imbue(locale(""));
+#endif
     if (argc < 2) 
 	   cout << "You can also use a number as an argument. Defaulting to 1,000,000"<<endl; 
     long long  upper_limit = ((argc > 1) ? strtoll(argv[1],&endp,10) : 1000000); // max n defaults to 1,000,000
     int num_threads = thread::hardware_concurrency(); // Use available hardware threads
-	cout.imbue(locale("")); // add seperators to numbers 
-    cout <<"Starting Collatz calculations from 1 up to "<<upper_limit<<" with "<<num_threads<< " threads."<<endl;
+	cout <<"Starting Collatz calculations from 1 up to "<<upper_limit<<" with "<<num_threads<< " threads."<<endl;
+
     vector<thread> threads;
     vector<long long> local_max_ns(num_threads); // one bucket per thread 
     vector<long long> local_max_steps(num_threads);
@@ -69,7 +78,6 @@ int main(int argc, char** argv) {
             max_n = local_max_ns[i];
         }
     }
-    cout<<"Longest chain found for number "<<max_n<<" with length "<<max_steps<<endl;
-    
+	cout<<"Longest chain found for number "<<max_n<<" with length "<<max_steps<<endl;
     return 0;
 }
